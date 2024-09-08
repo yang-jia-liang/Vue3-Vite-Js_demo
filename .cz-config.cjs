e1404404b1fbeb133567c664ea3a163d46223e89
yangjialiang <528748995@qq.com>
2024-09-08 19:21:14 +0800

    update: 1921

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -17,6 +17,7 @@ module.exports = {
   messages: {
     type: '选择一种你提交的类型(必选):',
     scope: '\n自定义更改范围(可选):',
+    customScope: 'What is the scope of this change:',
     subject: '短说明(必填):\n',
     body: '长说明, 使用"|"换行(可选):\n',
     breaking: '列举非兼容性的重大变更(可选):\n',
@@ -25,6 +26,12 @@ module.exports = {
   },
 
   allowBreakingChanges: ['update', 'feat', 'fix'],
+
+  skipQuestions: ['scope'],
+  defaultScope: '___CUSTOM___:',
+
+  allowCustomScopes: true,
+  allowEmptyScopes: true,
 
   // 自定义 `scope` 提示文本
   customScopes: {
--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,5 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     'type-enum': [0],
-    'scope-empty': [0, 'never'], // 允许没有 scope
   },
 };
